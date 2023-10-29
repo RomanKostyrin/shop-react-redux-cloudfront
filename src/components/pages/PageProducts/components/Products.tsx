296e6111c51f0b775a9f0c0f9c47b5756f24b04e
unknown <enemy-iubip@mail.ru>
2023-10-28 20:33:04 -0700

    feat: add theme for shop

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -6,30 +6,32 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { formatAsPrice } from "~/utils/utils";
 import AddProductToCart from "~/components/AddProductToCart/AddProductToCart";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 import { useAvailableProducts } from "~/queries/products";
 
 export default function Products() {
-  const { data = [], isLoading } = useAvailableProducts();
+  const [searchParams] = useSearchParams();
+  const filter = searchParams.get("filter") || "";
+  const { data = [], isLoading, refetch } = useAvailableProducts(filter);
+
+  useEffect(() => {
+    refetch();
+  }, [filter]);
 
   if (isLoading) {
     return <Typography>Loading...</Typography>;
   }
 
+  if (data.length === 0) return <Typography>No data</Typography>;
   return (
-    <Grid container spacing={4}>
-      {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
-      {data.map(({ count, ...product }, index) => (
+    <Grid container spacing={2}>
+      {data.map((product) => (
         <Grid item key={product.id} xs={12} sm={6} md={4}>
-          <Card
-            sx={{ height: "100%", display: "flex", flexDirection: "column" }}
-          >
-            <CardMedia
-              sx={{ pt: "56.25%" }}
-              image={`https://source.unsplash.com/random?sig=${index}`}
-              title="Image title"
-            />
+          <Card sx={{ height: "100%", display: "flex", flexDirection: "column" }}>
+            <CardMedia component="img" height="250" image={`/images/${product.id}.jpg`} title={product.id} />
             <CardContent sx={{ flexGrow: 1 }}>
-              <Typography gutterBottom variant="h5" component="h2">
+              <Typography gutterBottom variant="h6" component="h2">
                 {product.title}
               </Typography>
               <Typography>{formatAsPrice(product.price)}</Typography>
